bd7991884170aa44d4256058b32b7088f1ad899c
EnRoermyCx <119956950+EnRoermyCx@users.noreply.github.com>
2022-12-06 16:54:16 +0100

    Add files via upload

---
+++ PSPlus.bat
@@ -0,0 +1,494 @@
+@echo off
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PS4 Free Subscription PlayStation Plus 
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo Made by EnRoermyCx
+cls
+
+color 3
+cls
+echo Sign up with an account you own on PS4...
+echo.
+set /p user=Username:
+set /p pass=Password:
+cls
+echo the program is now checking username and password, please confirm if it is correct...
+echo ---------------------------USERNAME: %user%----------------------------- 
+echo ---------------------------USERNAME: %user%----------------------------- 
+echo ---------------------------USERNAME: %user%----------------------------- 
+echo ---------------------------USERNAME: %user%----------------------------- 
+echo ---------------------------USERNAME: %user%----------------------------- 
+echo ---------------------------USERNAME: %user%----------------------------- 
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBPY5PGBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY  .:^~7?Y5GGBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY          .:~7J5PGBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY                .:~7YPGBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY                     .^7YGBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY             ~!~.        :7PBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            !BBB5.         .?BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            JBBBB!           !BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            JBBBB!            ?BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            JBBBB!            .PBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            JBBBB!             YBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            JBBBB!             ?BBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            JBBBB!             7BBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            JBBBB!             ?BBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            JBBBB!            .PBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            JBBBB!            7BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY            JBBBB!          .?BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBG5GBY            JBBBBJ^:.    .^7PBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBGPY?!^. YBY            JBBBBBBGPP55PGBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBGP5J7~:.      YBY            JBBBBGGP5YJ?7!!~~~~~!!7?JY5PGBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBGPY?~:.            YBY            JBP7~:.                    .:~7J5GBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBPJ7^:              .:~7PBY            JBY             .^~!!!!~:        :!5BBBBBBBBBBBBBBB
+echo BBBBBBBBBBP7^.             .^!?YPGBBBBY            JB5       .^~?YPGBBBBBBGY.          YBBBBBBBBBBBBBBB
+echo BBBBBBBBB5.           :~7J5GBBBBBBG5GBY            JBY .:~7J5PGBBBBBGPY?!^.           :PBBBBBBBBBBBBB
+echo BBBBBBBBB5:          7GBBBBBGP5J7^: YBY            JBPYPGBBBBBBG5J7~:.             .~JGBBBBBBBBBBBBBB
+echo BBBBBBBBBBP?^.       .:^^^^^:.      YBY            JBBBBBGPY?!^.              .^!?5GBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBP5J7~^:.               YBY            JBGJ7~:.             .:~7J5GBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBGPP5YJJ?????JJY5PGBY            JBY             .^!?YPGBBBBBBBGPPPGBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBY.           JB5       .:~7J5GBBBBBBBBBBBBGY???JPBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBGG5Y?!^:.    JBY  .^!?YPGBBBBBBBBBBBBBBBBBGJ?Y?JPBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBGPY?!^YBPJ5GBBBBBBBBBBBBBBBBBBBBBBBBGP55GBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBGBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+echo ---------------------------PASSWORD: %pass%-----------------------------
+echo ---------------------------PASSWORD: %pass%-----------------------------
+echo ---------------------------PASSWORD: %pass%-----------------------------
+echo ---------------------------PASSWORD: %pass%-----------------------------
+echo ---------------------------PASSWORD: %pass%-----------------------------
+echo ---------------------------PASSWORD: %pass%----------------------------- 
+echo.
+timeout /t 10 /nobreak
+cls
+:A
+echo Please Login
+echo.
+set /p u=Username:
+set /p p=Password:
+if %u%==%user% if %p%==%pass% goto B
+cls
+echo Please try again!
+echo.
+pause
+cls
+goto A
+:B
+cls
+echo The program is currently linked to your PS account. Username: %user% Password: %pass%
+echo.
+timeout /t 10 /nobreak
+
+:A
+cls
+echo.
+echo.
+set /p menu="How long you want have PSplus?(MONTHS) (1/3/12): "
+if %menu%==1 goto 1
+if %menu%==1 goto 1
+if %menu%==3 goto 3
+if %menu%==3 goto 3
+if %menu%==12 goto 12
+if %menu%==12 goto 12
+cls
+echo.
+echo.
+set /p pause="Press any key to continue!... "
+goto A
+
+:1
+cls
+echo.
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo The program added:
+echo access %pass%
+echo config.dll
+echo PS.dll
+echo PS4config.dll
+echo PSaccountVision.dll
+echo PS4.img
+echo PSplus1Month
+echo PlayerID %user%.dll
+echo F5FAX5.code
+echo Username// %user%.img 
+pause
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+echo.
+echo.
+set /p pause="Press any key to continue!... "
+exit
+
+:3
+cls
+echo.
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo The program added:
+echo access %pass%
+echo config.dll
+echo PS.dll
+echo PS4config.dll
+echo PSaccountVision.dll
+echo PS4.img
+echo PSplus3Months
+echo PlayerID %user%.dll
+echo ILCS7V.code
+echo Username// %user%.img 
+pause
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+echo.
+echo.
+set /p pause="Press any key to continue!... "
+exit
+
+
+:12
+cls
+echo.
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo The program added:
+echo access %pass%
+echo config.dll
+echo PS.dll
+echo PS4config.dll
+echo PSaccountVision.dll
+echo PS4.img
+echo PSplus12Months
+echo PlayerID %user%.dll
+echo DWS84.code
+echo Username// %user%.img 
+pause
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+echo.
+cls
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo PSplus Has Successfully Added!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+:loop
+set /a num=(%random% %%9)+1
+color %num%
+echo CLOSE THIS WINDOW!
+goto :loop
+
+
